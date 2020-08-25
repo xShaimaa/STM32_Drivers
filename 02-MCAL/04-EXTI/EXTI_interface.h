2582928070f48ccac6f91ce1e7c4f08d7be4db75
xShaimaa <xshimoo@gmail.com>
2020-08-26 01:42:36 +0200

    EXTI Driver

--- 02-MCAL/04-EXTI/EXTI_interface.h
+++ 02-MCAL/04-EXTI/EXTI_interface.h
@@ -6,13 +6,36 @@
 #ifndef EXTI_INTERFACE_H
 #define EXTI_INTERFACE_H
 
+
+/* interrupt events */
+#define	EXTI_RISING_EDGE  0
+#define	EXTI_FALLING_EDGE 1
+#define	EXTI_ON_CHANGE	  2
+
+
 /* public functions prototypes */
 
 /******************************************************************************
-* Description : getting the active flag statues
-* Parameters  : Interrupt number (1 -> 59)
-* Return type : u8 (the active flag)
+* Description : enabling the external interrupt for a line and setting the corresponding event
+* Parameters  : Line number (0->15), IRQ event type (Rising or falling edge or on change)
+* Return type : void 
 ******************************************************************************/
+void MEXTI_voidEnableEXTI(u8 Copy_u8Line, u8 Copy_u8IRQEvent);
+
+/******************************************************************************
+* Description : disabling the external interrupt for a line
+* Parameters  : Line number (0->15) 
+* Return type : void 
+******************************************************************************/
+void MEXTI_voidDisableEXTI(u8 Copy_u8Line);
+
+/******************************************************************************
+* Description : setting the software external interrupt for a line
+* Parameters  : Line number (0->15) 
+* Return type : void 
+******************************************************************************/
+void MEXTI_voidEnableSW(u8 Copy_u8Line);
+
 
 
 #endif
--- 02-MCAL/04-EXTI/EXTI_program.c
+++ 02-MCAL/04-EXTI/EXTI_program.c
@@ -9,3 +9,44 @@
 #include "EXTI_interface.h"
 #include "EXTI_private.h"
 #include "EXTI_config.h"
+
+void MEXTI_voidEnableEXTI(u8 Copy_u8Line, u8 Copy_u8IRQEvent)
+{
+	/* unmasking the IRQ line */
+	SET_BIT(EXTI -> EXTI_IMR, Copy_u8Line);
+	/* setting the eent configurations */
+	switch(Copy_u8IRQEvent)
+	{
+		case EXTI_RISING_EDGE	:
+						/* enabling the rising trigger and disabling the falling trigger*/
+						SET_BIT(EXTI -> EXTI_RTSR, Copy_u8Line);
+						CLR_BIT(EXTI ->	EXTI_FTSR, Copy_u8Line);
+						break;
+						
+		case EXTI_FALLING_EDGE	:
+						/* enabling the rising trigger and disabling the falling trigger*/
+						SET_BIT(EXTI -> EXTI_FTSR, Copy_u8Line);
+						CLR_BIT(EXTI ->	EXTI_RTSR, Copy_u8Line);
+						break;
+						
+		case EXTI_ON_CHANGE		:
+						/* enabling both the rising and the falling trigger*/
+						SET_BIT(EXTI -> EXTI_FTSR, Copy_u8Line);
+						SET_BIT(EXTI ->	EXTI_RTSR, Copy_u8Line);
+						break;
+						
+		default		:	break;
+	}
+}
+
+void MEXTI_voidDisableEXTI(u8 Copy_u8Line)
+{
+	/* masking the IRQ line */
+	CLR_BIT(EXTI -> EXTI_IMR, copy_u8Line); 
+}
+
+
+void MEXTI_voidEnableSW(u8 Copy_u8Line)
+{
+	SET_BIT(EXTI -> EXTI_SWIER, SET_BIT(EXTI -> EXTI_IMR, Copy_u8Line));
+}
