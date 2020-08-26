1e5a6ecebbd0085aba6b6d7cfc954d03eaa020a7
xShaimaa <xshimoo@gmail.com>
2020-08-26 04:10:08 +0200

    EXTI Updated

--- 02-MCAL/04-EXTI/EXTI_private.h
+++ 02-MCAL/04-EXTI/EXTI_private.h
@@ -8,12 +8,12 @@
 
 /* Register Definitions */
 typedef struct{
-	volatile u32 EXTI_IMR 	; 
-	volatile u32 EXTI_EMR 	; 
-	volatile u32 EXTI_RTSR  ; 
-	volatile u32 EXTI_FTSR  ; 
-	volatile u32 EXTI_SWIER ; 
-	volatile u32 EXTI_PR 	; 
+	volatile u32 EXTI_IMR 	;		/* masks or unmasks the IRQ */ 
+	volatile u32 EXTI_EMR 	; 		/* masks or unmasks the IRQ event*/
+	volatile u32 EXTI_RTSR  ;		/* rising  trigger selection */ 
+	volatile u32 EXTI_FTSR  ;		/* falling trigger selection */ 
+	volatile u32 EXTI_SWIER ;		/* controls the software interrupt event*/ 
+	volatile u32 EXTI_PR 	; 		/* pending register */
 
 }EXTI_t;
 
