d29d1eae9616af891e0c461d84ea6c48e82f93b1
xShaimaa <xshimoo@gmail.com>
2020-08-31 01:33:25 +0200

    NVIC Driver Updated

--- 02-MCAL/03-NVIC/NVIC_interface.h
+++ 02-MCAL/03-NVIC/NVIC_interface.h
@@ -6,6 +6,70 @@
 #ifndef NVIC_INTERFACE_H
 #define NVIC_INTERFACE_H
 
+/* Data type holding external interrupts */
+typedef enum{
+		WWDG_IRQ
+		TAMPER_IRQ
+		RTC_IRQ
+		FLASH_IRQ
+		RCC_IRQ
+		EXTI0_IRQ
+		EXTI1_IRQ
+		EXTI2_IRQ
+		EXTI3_IRQ
+		EXTI4_IRQ
+		DMA1_Channel1_IRQ
+		DMA1_Channel2_IRQ
+		DMA1_Channel3_IRQ
+		DMA1_Channel4_IRQ
+		DMA1_Channel5_IRQ
+		DMA1_Channel6_IRQ
+		DMA1_Channel7_IRQ
+		ADC1_2_IRQ
+		CAN1_TX_IRQ
+		CAN1_RX0_IRQ
+		CAN1_RX1_IRQ
+		CAN1_SCE_IRQ
+		EXTI9_5_IRQ
+		TIM1_BRK_IRQ
+		TIM1_UP_IRQ
+		TIM1_TRG_COM_IRQ
+		TIM1_CC_IRQ
+		TIM2_IRQ
+		TIM3_IRQ
+		TIM4_IRQ
+		I2C1_EV_IRQ
+		I2C1_ER_IRQ
+		I2C2_EV_IRQ
+		I2C2_ER_IRQ
+		SPI1_IRQ
+		SPI2_IRQ
+		USART1_IRQ
+		USART2_IRQ
+		USART3_IRQ
+		EXTI15_10_IRQ
+		RTCAlarm_IRQ
+		USBWakeup_IRQ
+		TIM8_BRK_IRQ
+		TIM8_UP_IRQ
+		TIM8_TRG_COM_IRQ
+		TIM8_CC_IRQ
+		ADC3_IRQ
+		FSMC_IRQ
+		SDIO_IRQ
+		TIM5_IRQ
+		SPI3_IRQ
+		UART4_IRQ
+		UART5_IRQ
+		TIM6_IRQ
+		TIM7_IRQ
+		DMA2_Channel1_IRQ
+		DMA2_Channel2_IRQ
+		DMA2_Channel3_IRQ
+		DMA2_Channel4_5_IRQ
+}IRQ_t
+
+
 /* public functions prototypes */
 
 /******************************************************************************
@@ -17,45 +81,45 @@ void MNVIC_voidInit(void);
 
 /******************************************************************************
 * Description : enabling the interrupt
-* Parameters  : Interrupt number (0 -> 59)
+* Parameters  : IRQ name
 * Return type : void
 ******************************************************************************/
-void MNVIC_voidEnableIRQ(u8 Copy_u8IRQNum);
+void MNVIC_voidEnableIRQ(IRQ_t Copy_u8IRQName);
 
 /******************************************************************************
 * Description : disabling the interrupt
-* Parameters  : Interrupt number (0 -> 59)
+* Parameters  : IRQ name
 * Return type : void
 ******************************************************************************/
-void MNVIC_voidDisableIRQ(u8 Copy_u8IRQNum);
+void MNVIC_voidDisableIRQ(IRQ_t Copy_u8IRQName);
 
 /******************************************************************************
 * Description : setting the interrupt's pending flag
-* Parameters  : Interrupt number (1 -> 59)
+* Parameters  : IRQ name
 * Return type : void
 ******************************************************************************/
-void MNVIC_voidSetPendingFlag(u8 Copy_u8IRQNum);
+void MNVIC_voidSetPendingFlag(IRQ_t Copy_u8IRQName);
 
 /******************************************************************************
 * Description : clearing the interrupt's pending flag
-* Parameters  : Interrupt number (0 -> 59)
+* Parameters  : IRQ name
 * Return type : void
 ******************************************************************************/
-void MNVIC_voidClrPendingFlag(u8 Copy_u8IRQNum);
+void MNVIC_voidClrPendingFlag(IRQ_t Copy_u8IRQName);
 
 /******************************************************************************
 * Description : getting the active flag statues
-* Parameters  : Interrupt number (0 -> 59)
+* Parameters  : IRQ name
 * Return type : u8 (the active flag)
 ******************************************************************************/
-u8 MNVIC_u8GetActiveFlag(u8 Copy_u8IRQNum);
+u8 MNVIC_u8GetActiveFlag(IRQ_t Copy_u8IRQName);
 
 /******************************************************************************
 * Description : setting the interrupt priority
-* Parameters  : Interrupt number (0-> 59), Interrupt priority (0->15)
+* Parameters  : IRQ name, Interrupt priority (0->15)
 * Return type : void 
 ******************************************************************************/
-void MNVIC_voidSetPriorty(u8 Copy_u8IRQNum, u8 Copy_u8IRQPriority);
+void MNVIC_voidSetPriorty(IRQ_t Copy_u8IRQName, u8 Copy_u8IRQPriority);
 
 
 #endif
--- 02-MCAL/03-NVIC/NVIC_program.c
+++ 02-MCAL/03-NVIC/NVIC_program.c
@@ -17,91 +17,91 @@ void MNVIC_voidInit(void)
 }
 
 /* enabling the interrupt */
-void MNVIC_voidEnableIRQ(u8 Copy_u8IRQNum)
+void MNVIC_voidEnableIRQ(IRQ_t Copy_u8IRQName)
 {
-	if (Copy_u8IRQNum <= 31)
+	if (Copy_u8IRQName <= 31)
 	{
-		NVIC_ISER0 = (1 << Copy_u8IRQNum);
+		NVIC_ISER0 = (1 << Copy_u8IRQName);
 	}
 	
-	else if (Copy_u8IRQNum <= 59)
+	else if (Copy_u8IRQName <= 59)
 	{
-		Copy_u8IRQNum -= 32;
-		NVIC_ISER1 = (1 << Copy_u8IRQNum);
+		Copy_u8IRQName -= 32;
+		NVIC_ISER1 = (1 << Copy_u8IRQName);
 	}
 }
 
 /* disabling the interrupt */
-void MNVIC_voidDisableIRQ(u8 Copy_u8IRQNum)
+void MNVIC_voidDisableIRQ(IRQ_t Copy_u8IRQName)
 {
-	if (Copy_u8IRQNum <= 31)
+	if (Copy_u8IRQName <= 31)
 	{
-		NVIC_ICER0 = (1 << Copy_u8IRQNum);
+		NVIC_ICER0 = (1 << Copy_u8IRQName);
 	}
 	
-	else if (Copy_u8IRQNum <= 59)
+	else if (Copy_u8IRQName <= 59)
 	{
-		Copy_u8IRQNum -= 32;
-		NVIC_ICER1 = (1 << Copy_u8IRQNum);
+		Copy_u8IRQName -= 32;
+		NVIC_ICER1 = (1 << Copy_u8IRQName);
 	}
 }
 
 /* setting the interrupt's pending flag */
-void MNVIC_voidSetPendingFlag(u8 Copy_u8IRQNum)
+void MNVIC_voidSetPendingFlag(IRQ_t Copy_u8IRQName)
 {
-	if (Copy_u8IRQNum <= 31)
+	if (Copy_u8IRQName <= 31)
 	{
-		NVIC_ISPR0 = (1 << Copy_u8IRQNum);
+		NVIC_ISPR0 = (1 << Copy_u8IRQName);
 	}
 	
-	else if (Copy_u8IRQNum <= 59)
+	else if (Copy_u8IRQName <= 59)
 	{
-		Copy_u8IRQNum -= 32;
-		NVIC_ISPR1 = (1 << Copy_u8IRQNum);
+		Copy_u8IRQName -= 32;
+		NVIC_ISPR1 = (1 << Copy_u8IRQName);
 	}
 }
 
 /* clearing the interrupt's pending flag */
-void MNVIC_voidClrPendingFlag(u8 Copy_u8IRQNum)
+void MNVIC_voidClrPendingFlag(IRQ_t Copy_u8IRQName)
 {
-	if (Copy_u8IRQNum <= 31)
+	if (Copy_u8IRQName <= 31)
 	{
-		NVIC_ICPR0 = (1 << Copy_u8IRQNum);
+		NVIC_ICPR0 = (1 << Copy_u8IRQName);
 	}
 	
-	else if (Copy_u8IRQNum <= 59)
+	else if (Copy_u8IRQName <= 59)
 	{
-		Copy_u8IRQNum -= 32;
-		NVIC_ICPR1 = (1 << Copy_u8IRQNum);
+		Copy_u8IRQName -= 32;
+		NVIC_ICPR1 = (1 << Copy_u8IRQName);
 	}
 }
 
 
 /* getting the active flag statues */
-u8 MNVIC_u8GetActiveFlag(u8 Copy_u8IRQNum)
+u8 MNVIC_u8GetActiveFlag(IRQ_t Copy_u8IRQName)
 {
 	u8 Local_u8Result;
 	
-	if (Copy_u8IRQNum <= 31)
+	if (Copy_u8IRQName <= 31)
 	{
-		Local_u8Result = GET_BIT(NVIC_IABR0,Copy_u8IRQNum);
+		Local_u8Result = GET_BIT(NVIC_IABR0, Copy_u8IRQName);
 	}
 	
-	else if (Copy_u8IRQNum <= 59)
+	else if (Copy_u8IRQName <= 59)
 	{
-		Copy_u8IRQNum -= 32;
-		Local_u8Result = GET_BIT(NVIC_IABR1,Copy_u8IRQNum);
+		Copy_u8IRQName -= 32;
+		Local_u8Result = GET_BIT(NVIC_IABR1, Copy_u8IRQName);
 	}
 	
 	return Local_u8Result;
 }
 
 /* setting the interrupt priority */
-void MNVIC_voidSetPriorty(u8 Copy_u8IRQNum, u8 Copy_u8IRQPriority)
+void MNVIC_voidSetPriorty(IRQ_t Copy_u8IRQName, u8 Copy_u8IRQPriority)
 {
-	if(Copy_u8IRQNum < 60)
+	if(Copy_u8IRQName < 60)
 	{
-		NVIC_IPR[Copy_u8IRQNum] = (Copy_u8IRQPriority << 4);
+		NVIC_IPR[Copy_u8IRQName] = (Copy_u8IRQPriority << 4);
 	}
 	
 	else {
