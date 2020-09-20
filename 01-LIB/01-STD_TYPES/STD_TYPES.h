a2052e3259615623a961b912bdd0af1bcef42c8c
xShaimaa <xshimoo@gmail.com>
2020-09-20 19:13:18 +0200

    STD_TYPES v02

--- 01-LIB/01-STD_TYPES/STD_TYPES.h
+++ 01-LIB/01-STD_TYPES/STD_TYPES.h
@@ -1,14 +1,25 @@
 /****************************/
 /* Auther	: Shimo			*/
 /* Date  	: 29 JUL 2020	*/
-/* Version	: V01			*/
+/* Version	: V02			*/
 /****************************/
 #ifndef STD_TYPES_H
 #define STD_TYEPS_H
 
-typedef unsigned char 		u8 ; 
-typedef unsigned short int 	u16; 
-typedef unsigned long int 	u32; 
+/* unsigned data types */
+typedef unsigned char		u8 ;
+typedef unsigned short int  u16;
+typedef unsigned long int 	u32;
+typedef unsigned long long  u64;
 
+/* signed data types */
+typedef signed char			s8 ;
+typedef signed short int  	s16;
+typedef signed long int 	s32;
+typedef signed long long 	s64;
+
+/* floating data types */
+typedef float  				f32;
+typedef double 				f64;
 
 #endif
